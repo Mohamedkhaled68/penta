754c6541ecbdde6d494521f6a028a3e690a566bd
Mohamedkhaled68 <mohamed.khaled6083@gmail.com>
2026-01-17 17:08:55 +0200

    nu

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+        formats: ["image/webp", "image/avif"],
         unoptimized: true, // Disable image optimization for static export or unsupported hosts
     },
 };
